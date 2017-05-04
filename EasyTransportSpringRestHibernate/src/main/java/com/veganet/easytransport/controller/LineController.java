5bb84c60fedc1ee3f3d94bd33c9fda20f4ad8b6f
nesrinesk <skjnesrine@gmail.com>
2017-05-04 11:18:50 +0100

    LineController.java

--- EasyTransportSpringRestHibernate/src/main/java/com/veganet/easytransport/controller/LineController.java
+++ EasyTransportSpringRestHibernate/src/main/java/com/veganet/easytransport/controller/LineController.java
@@ -7,6 +7,7 @@ package com.veganet.easytransport.controller;
 
 import com.veganet.easytransport.entities.Line;
 import com.veganet.easytransport.service.LineService;
+import java.math.BigInteger;
 import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
@@ -42,13 +43,13 @@ public class LineController {
         }
         return listOfLines;
     }
-    
+
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     @RequestMapping(value = "/getLine/{id}", method = RequestMethod.GET)
     public Line getLineById(@PathVariable int id) {
-        logger.info("getting line with id :"+id);
-        
+        logger.info("getting line with id :" + id);
+
         return lineService.findOne(id);
     }
 
@@ -60,28 +61,27 @@ public class LineController {
         lineService.create(line);
 
     }
+
     @Consumes(MediaType.APPLICATION_JSON)
 
     @RequestMapping(value = "/updateLine/{id}", method = RequestMethod.POST)
-    public void updateLine(@PathVariable int id,@RequestBody Line line) {
-       
+    public void updateLine(@PathVariable int id, @RequestBody Line line) {
+
         Line currentLine = lineService.findOne(id);
-         logger.info("updating line with id :"+id);
-          if (currentLine==null) {
-               logger.info("Line with id {} not found"+ id);
-           }
-          lineService.update2(line);
-          logger.info("updated line with id :"+id);
+        logger.info("updating line with id :" + id);
+        if (currentLine == null) {
+            logger.info("Line with id {} not found" + id);
         }
-    
+        lineService.update2(line);
+        logger.info("updated line with id :" + id);
+    }
 
     @RequestMapping(value = "/deleteLine/{id}", method = RequestMethod.DELETE, headers = "Accept=application/json")
     public void deleteLine(@PathVariable("id") int id) {
         lineService.deleteById(id);
     }
-    
-    
-     //not deleted users
+
+    //not deleted users
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     @RequestMapping(value = "/getAll", method = RequestMethod.GET)
@@ -94,25 +94,25 @@ public class LineController {
         }
         return list;
     }
+
     //add
+
     @Consumes(MediaType.APPLICATION_JSON)
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     public void add(@RequestBody Line line) {
         logger.info("creating new : {}");
-
         lineService.add(line);
+        logger.info("id : {}"+line.getLineId());
 
     }
-    
-//delete setting isdeleted = 1
 
+//delete setting isdeleted = 1
     @RequestMapping(value = "/delete2/{id}", method = RequestMethod.POST, headers = "Accept=application/json")
     public void delete2(@PathVariable("id") int id) {
         lineService.delete2(id);
     }
-    
+
     //trains
-   
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     @RequestMapping(value = "/getTrainLines", method = RequestMethod.GET)
@@ -125,10 +125,9 @@ public class LineController {
         }
         return list;
     }
-    
-    
+
     //bus
-     @Produces(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     @RequestMapping(value = "/getBusLines", method = RequestMethod.GET)
     public List<Line> getBusLines() {
@@ -140,4 +139,16 @@ public class LineController {
         }
         return list;
     }
+
+    //getLastInsertedId
+    /*@Produces(MediaType.APPLICATION_JSON)
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @RequestMapping(value = "/getLastInsertedId", method = RequestMethod.GET)
+    public BigInteger getLastInsertedId() {
+        logger.info("getting last inseted id");
+
+        BigInteger id = lineService.getLastInsertedId();
+
+        return id;
+    }*/
 }
