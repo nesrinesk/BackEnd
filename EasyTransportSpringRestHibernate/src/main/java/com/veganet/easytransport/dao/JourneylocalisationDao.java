30ea9cef7e5ad7783b225bfa74d35e8a6708dcdb
nesrinesk <skjnesrine@gmail.com>
2017-05-08 12:33:38 +0100

    JourneylocalisationDao

--- EasyTransportSpringRestHibernate/src/main/java/com/veganet/easytransport/dao/JourneylocalisationDao.java
+++ EasyTransportSpringRestHibernate/src/main/java/com/veganet/easytransport/dao/JourneylocalisationDao.java
@@ -6,6 +6,10 @@
 package com.veganet.easytransport.dao;
 
 import com.veganet.easytransport.entities.Journeylocalisation;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -13,9 +17,24 @@ import org.springframework.stereotype.Repository;
  * @author asus
  */
 @Repository
-public class JourneylocalisationDao extends AbstractHibernateDao<Journeylocalisation>{
+public class JourneylocalisationDao extends AbstractHibernateDao<Journeylocalisation> {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public void setSessionFactory(SessionFactory sf) {
+        this.sessionFactory = sf;
+    }
 
     public JourneylocalisationDao() {
         setClazz(Journeylocalisation.class);
+    }
+
+    public List<Journeylocalisation> getAllByType(short type) {
+        Session session = this.sessionFactory.getCurrentSession();
+        List<Journeylocalisation> list = session.createQuery("SELECT l FROM Journeylocalisation l WHERE l.type = :type")
+                .setParameter("type", type).list();
+        return list;
     }
 }
