80454c7267be6e7fc5f692723fe457e921f542dc
nesrinesk <skjnesrine@gmail.com>
2017-05-25 16:28:21 +0100

    LoginController

---
+++ EasyTransportSpringRestHibernate/src/main/java/com/veganet/easytransport/controller/LoginController.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.veganet.easytransport.controller;
+
+import com.veganet.easytransport.entities.User;
+import com.veganet.easytransport.service.UserService;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author asus
+ */
+@RestController
+@RequestMapping(value = "/security")
+public class LoginController {
+
+    @Resource(name = "authenticationManager")
+    private AuthenticationManager authenticationManager; // specific for Spring Security
+
+    private static final org.jboss.logging.Logger logger = org.jboss.logging.Logger.getLogger(LoginController.class);
+
+    @Autowired
+    UserService userService;
+
+    String connectedUserName;
+
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @RequestMapping(value = "/login/{username}/{password}", method = RequestMethod.GET)
+    private void doAutoLogin(@PathVariable String username, @PathVariable String password, HttpServletRequest request) {
+
+        try {
+            // Must be called from request filtered by Spring Security, otherwise SecurityContextHolder is not updated
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
+            token.setDetails(new WebAuthenticationDetails(request));
+            Authentication authentication = this.authenticationManager.authenticate(token);
+            logger.info("Logging in with [{}]" + authentication.getPrincipal());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            connectedUserName = username;
+            logger.info("connectedUserName " + connectedUserName);
+
+        } catch (Exception e) {
+            SecurityContextHolder.getContext().setAuthentication(null);
+            logger.error("Failure in autoLogin", e);
+        }
+
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public void logoutPage(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+            connectedUserName="";
+        }
+        logger.info("logout");
+    }
+
+//findOne by username
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @RequestMapping(value = "/findConnectedUser", method = RequestMethod.GET)
+    public User findByUserName (){
+        logger.info("getting user with id :" + connectedUserName);
+        return userService.findByUserName(connectedUserName);
+    }
+
+}
