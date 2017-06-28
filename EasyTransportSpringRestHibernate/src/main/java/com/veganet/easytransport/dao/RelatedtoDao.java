dfbc045d1425c8b5dd637469ba226e450b851077
nesrinesk <skjnesrine@gmail.com>
2017-06-28 12:15:18 +0100

    RelatedtoDao

--- EasyTransportSpringRestHibernate/src/main/java/com/veganet/easytransport/dao/RelatedtoDao.java
+++ EasyTransportSpringRestHibernate/src/main/java/com/veganet/easytransport/dao/RelatedtoDao.java
@@ -5,60 +5,18 @@
  */
 package com.veganet.easytransport.dao;
 
-import com.veganet.easytransport.entities.Line;
 import com.veganet.easytransport.entities.Relatedto;
 import java.util.List;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author asus
  */
-@Repository
-public class RelatedtoDao extends AbstractHibernateDao<Relatedto> {
+public interface RelatedtoDao {
 
-    @Autowired
-    private SessionFactory sessionFactory;
+    public List<Relatedto> getAllByType(short type);
 
-    @Override
-    public void setSessionFactory(SessionFactory sf) {
-        this.sessionFactory = sf;
-    }
+    public List<Relatedto> getAllByLine(int id);
 
-    public RelatedtoDao() {
-        setClazz(Relatedto.class);
-    }
-
-    // by  type
-    public List<Relatedto> getAllByType(short type) {
-        Session session = this.sessionFactory.getCurrentSession();
-        List<Relatedto> list = session.createQuery("SELECT r FROM Relatedto r WHERE r.type = :type")
-                .setParameter("type", type).list();
-        return list;
-    }
-
-    // by  lineId
-    public List<Relatedto> getAllByLine(int id) {
-        Session session = this.sessionFactory.getCurrentSession();
-        Line lineId = (Line) session.get(Line.class, id);
-        List<Relatedto> list = session.createQuery("SELECT r FROM Relatedto r WHERE r.lineId = :lineId")
-                .setParameter("lineId", lineId).list();
-        return list;
-    }
-    /*
-     Session session = this.sessionFactory.getCurrentSession();
-     T object = (T) session.get(clazz, id);
-     return object;
-     */
-
-    public Relatedto getlaststation(short tag, int id) {
-        Session session = this.sessionFactory.getCurrentSession();
-        Line lineId = (Line) session.get(Line.class, id);
-        Relatedto object = (Relatedto) session.createQuery("SELECT r FROM Relatedto r WHERE r.tag = :tag and r.lineId = :lineId ")
-                .setParameter("tag", tag).setParameter("lineId", lineId).uniqueResult();
-        return object;
-    }
+    public Relatedto getlaststation(short tag, int id);
 }
