9dbc2568d561fb26dc29a27669d3f5244e402f56
nesrinesk <skjnesrine@gmail.com>
2017-06-28 12:12:47 +0100

    StationDaoImpl

---
+++ EasyTransportSpringRestHibernate/src/main/java/com/veganet/easytransport/dao/impl/StationDaoImpl.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.veganet.easytransport.dao.impl;
+
+import com.veganet.easytransport.dao.StationDao;
+import com.veganet.easytransport.dao.impl.AbstractHibernateDao;
+import com.veganet.easytransport.entities.Station;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author asus
+ */
+@Repository
+public class StationDaoImpl extends AbstractHibernateDao<Station> implements StationDao{
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public void setSessionFactory(SessionFactory sf) {
+        this.sessionFactory = sf;
+    }
+
+    public StationDaoImpl() {
+        setClazz(Station.class);
+    }
+
+    //add+ set isdeleted =0
+    @Override
+    public Station add(Station station) {
+        Session session = this.sessionFactory.getCurrentSession();
+        session.persist(station);
+        station.setIsdeleted((short) 0);
+        return station;
+    }
+
+    //users not deleted (having isDeleted=0)
+    @Override
+    public List<Station> getAll(short isdeleted) {
+        Session session = this.sessionFactory.getCurrentSession();
+        List<Station> list = session.createQuery("SELECT s FROM Station s WHERE s.isdeleted = :isdeleted").setParameter("isdeleted", isdeleted).list();
+        return list;
+    }
+
+    // set isdeleted=1
+    @Override
+    public void delete2(int id) {
+        Session session = this.sessionFactory.getCurrentSession();
+        Station station = (Station) session.get(Station.class, id);
+        station.setIsdeleted((short) 1);
+
+    }
+
+    //update 
+
+    @Override
+    public void update2(Station object) {
+        Session session = this.sessionFactory.getCurrentSession();
+        session.update(object);
+        object.setIsdeleted((short) 0);
+    }
+
+    // Stations not deleted (type=0) by  type
+
+    @Override
+    public List<Station> getAllByType(short type, short isdeleted) {
+        Session session = this.sessionFactory.getCurrentSession();
+        List<Station> list = session.createQuery("SELECT s FROM Station s WHERE s.type = :type and s.isdeleted = :isdeleted")
+                .setParameter("type", type)
+                .setParameter("isdeleted", isdeleted).list();
+        return list;
+    }
+
+}
