7e6164951dab9c7ee6bb93c5aa7df899ee0a1be7
nesrinesk <skjnesrine@gmail.com>
2017-06-28 12:17:53 +0100

    AlertDao

--- EasyTransportSpringRestHibernate/src/main/java/com/veganet/easytransport/dao/AlertDao.java
+++ EasyTransportSpringRestHibernate/src/main/java/com/veganet/easytransport/dao/AlertDao.java
@@ -5,22 +5,10 @@
  */
 package com.veganet.easytransport.dao;
 
-import com.veganet.easytransport.entities.Alert;
-import java.util.List;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-
 /**
  *
  * @author asus
  */
-@Repository
-
-public class AlertDao extends AbstractHibernateDao<Alert>{
-
-    public AlertDao() {
-        setClazz(Alert.class);
-    }
+public interface AlertDao {
+    
 }
