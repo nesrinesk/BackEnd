4a2bbe38baef394aa49321d5c4703657ed3befd0
nesrinesk <skjnesrine@gmail.com>
2017-06-28 12:15:42 +0100

    LineDao

--- EasyTransportSpringRestHibernate/src/main/java/com/veganet/easytransport/dao/LineDao.java
+++ EasyTransportSpringRestHibernate/src/main/java/com/veganet/easytransport/dao/LineDao.java
@@ -6,72 +6,22 @@
 package com.veganet.easytransport.dao;
 
 import com.veganet.easytransport.entities.Line;
-import java.math.BigInteger;
 import java.util.List;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author asus
  */
-@Repository
-public class LineDao extends AbstractHibernateDao<Line>{
-     @Autowired
-    private SessionFactory sessionFactory;
+public interface LineDao {
 
-     @Override
-    public void setSessionFactory(SessionFactory sf) {
-        this.sessionFactory = sf;
-    }
-    public LineDao() {
-        setClazz(Line.class);
-    }
-    
-     //add+ set isdeleted =0
-    public Line add(Line line) {
-        Session session = this.sessionFactory.getCurrentSession();
-        session.persist(line);
-        session.flush();
-        
-        return line;
-    }
+    public Line add(Line line);
 
-    //users not deleted (having isDeleted=0)
-    public List<Line> getAll(short isdeleted) {
-        Session session = this.sessionFactory.getCurrentSession();
-        List<Line> list = session.createQuery("SELECT l FROM Line l WHERE l.isdeleted = :isdeleted").setParameter("isdeleted", isdeleted).list();
-        return list;
-    }
-    // set isdeleted=1
-    public void delete2(int id) {
-        Session session = this.sessionFactory.getCurrentSession();
-        Line line = (Line) session.get(Line.class, id);
-        line.setIsdeleted((short) 1);
+    public List<Line> getAll(short isdeleted);
 
-    }
-    //update 
-    public void update2(Line object) {
-        Session session = this.sessionFactory.getCurrentSession();
-        session.update(object);
-        object.setIsdeleted((short) 0);
-    }
-    
-    // Lines not deleted (type=0) by  type
-    public List<Line> getAllByType(short type, short isdeleted) {
-        Session session = this.sessionFactory.getCurrentSession();
-        List<Line> list = session.createQuery("SELECT l FROM Line l WHERE l.type = :type and l.isdeleted = :isdeleted")
-                .setParameter("type", type)
-                .setParameter("isdeleted", isdeleted).list();
-        return list;
-    }
-    
-   /* public BigInteger getLastInsertedId(){
-                Session session = this.sessionFactory.getCurrentSession();
+    public void delete2(int id);
 
-        BigInteger lastId= (BigInteger) session.createSQLQuery("SELECT LAST_INSERT_ID()").uniqueResult();
-        return lastId;
-    }*/
+    public void update2(Line object);
+
+    public List<Line> getAllByType(short type, short isdeleted);
+
 }
