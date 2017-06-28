c12710fb0fb18b6e05d2e6657be8eb0c8cc31eb3
nesrinesk <skjnesrine@gmail.com>
2017-06-28 12:17:01 +0100

    JourneyDao

--- EasyTransportSpringRestHibernate/src/main/java/com/veganet/easytransport/dao/JourneyDao.java
+++ EasyTransportSpringRestHibernate/src/main/java/com/veganet/easytransport/dao/JourneyDao.java
@@ -7,65 +7,22 @@ package com.veganet.easytransport.dao;
 
 import com.veganet.easytransport.entities.Journey;
 import java.util.List;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author asus
  */
-@Repository
-public class JourneyDao extends AbstractHibernateDao<Journey> {
+public interface JourneyDao {
 
-    @Autowired
-    private SessionFactory sessionFactory;
+    public Journey add(Journey journey);
 
-    public void setSessionFactory(SessionFactory sf) {
-        this.sessionFactory = sf;
-    }
+    public List<Journey> getAll(short isdeleted);
 
-    public JourneyDao() {
-        setClazz(Journey.class);
-    }
+    public void delete2(int id);
 
-    //add+ set isdeleted =0
-    public Journey add(Journey journey) {
-        Session session = this.sessionFactory.getCurrentSession();
-        session.persist(journey);
-        session.flush();
-        journey.setIsdeleted((short) 0);
-        return journey;
-    }
+    public void update2(Journey object);
 
-    //users not deleted (having isDeleted=0)
-    public List<Journey> getAll(short isdeleted) {
-        Session session = this.sessionFactory.getCurrentSession();
-        List<Journey> list = session.createQuery("SELECT j FROM Journey j WHERE j.isdeleted = :isdeleted").setParameter("isdeleted", isdeleted).list();
-        return list;
-    }
+    public List<Journey> getAllByType(short type, short isdeleted);
 
-    // set isdeleted=1
-    public void delete2(int id) {
-        Session session = this.sessionFactory.getCurrentSession();
-        Journey journey = (Journey) session.get(Journey.class, id);
-        journey.setIsdeleted((short) 1);
-
-    }
-    //update 
-    public void update2(Journey object) {
-        Session session = this.sessionFactory.getCurrentSession();
-        session.update(object);
-        object.setIsdeleted((short) 0);
-    }
-    
-    // Stations not deleted (type=0) by  type
-    public List<Journey> getAllByType(short type, short isdeleted) {
-        Session session = this.sessionFactory.getCurrentSession();
-        List<Journey> list = session.createQuery("SELECT j FROM Journey j WHERE j.type = :type and j.isdeleted = :isdeleted")
-                .setParameter("type", type)
-                .setParameter("isdeleted", isdeleted).list();
-        return list;
-    }
+    public String linesOfJourney(int journeyId);
 }
