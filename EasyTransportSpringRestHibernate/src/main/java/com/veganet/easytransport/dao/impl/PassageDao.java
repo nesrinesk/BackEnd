bb42d8c228f7ff220c4e527006bad279b2bbbf4f
nesrinesk <skjnesrine@gmail.com>
2017-06-28 12:14:03 +0100

    PassageDao

---
+++ EasyTransportSpringRestHibernate/src/main/java/com/veganet/easytransport/dao/impl/PassageDao.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.veganet.easytransport.dao.impl;
+
+import com.veganet.easytransport.dao.impl.AbstractHibernateDao;
+import com.veganet.easytransport.entities.Passage;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author asus
+ */
+@Repository
+public class PassageDao extends AbstractHibernateDao<Passage>{
+
+    public PassageDao() {
+        setClazz(Passage.class);
+    }
+}
